8dd14856b22e096cb7c86055c49efca5d93a2fc1
Ellie Lambert <ebezjian@gmail.com>
2020-10-30 12:29:14 -0400

    add for and else ifs to take const numArray numbers and replace with wordArray, test for functionality

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,15 @@ function count(num) {
   const newNum = num;
   const numArray = ["1","2","3"]
   const wordArray = ["Beep!", "Boop!", "Won't you be my neighbor?"];
-
+  for (let i = 0; i <= newNum; i++) {
+    if (i.toString().split('').includes(numArray[2])) {
+      userNum.push(wordArray[2]);
+    }else if(i.toString().split('').includes(numArray[1])) {
+      userNum.push(wordArray[1]);
+    }else if(i.toString().split('').includes(numArray[0])) {
+      userNum.push(wordArray[1]);
+    }else userNum.push(i);
+  }
+return (userNum + ", ");
+}
+});
