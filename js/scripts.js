c08e06bcdd620d7ff4cad9b20fe1c929588bba80
Ellie Lambert <ebezjian@gmail.com>
2020-10-30 13:13:22 -0400

    debugged reason why beep and boops weren't showing up, changed submit to keyup

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
   $("#num").keyup(function(event){
     event.preventDefault();
-    $("#result").html(count($("#num").val()));
+    $("#results").html(count($("#num").val()));
   })
   
 
@@ -15,15 +15,15 @@ function count(num) {
   const newNum = num;
   const numArray = ["1","2","3"]
   const wordArray = ["Beep!", "Boop!", "Won't you be my neighbor?"];
-  for (let i = 0; i <= newNum; i++) {
+  for (var i = 0; i <= newNum; i++) {
     if (i.toString().split('').includes(numArray[2])) {
       userNum.push(wordArray[2]);
     }else if(i.toString().split('').includes(numArray[1])) {
       userNum.push(wordArray[1]);
     }else if(i.toString().split('').includes(numArray[0])) {
-      userNum.push(wordArray[1]);
-    }else userNum.push(i);
-  }
-return (userNum + ", ");
+      userNum.push(wordArray[0]);
+    }else userNum.push(i);}
+    
+return (userNum + ",");
 }
 });
